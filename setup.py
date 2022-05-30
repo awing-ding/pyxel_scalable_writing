21130b576d87ed563cb3c32091a0d4a53563990e
awing_ding <awingding.win@gmail.com>
2022-05-30 22:54:13 +0200

    pypi versioning update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 
 setup (
     name = "scalablewritingpyxel",
-    version = "1.1.2",
+    version = "1.1.6",
     description = "A module that give a scalable text writing function for pyxel",
     long_description = "A module that give a scalable text writing function for pyxel, see README.md on github for documentation",
     url = "https://github.com/awing-ding/pyxel_scalable_writing",
