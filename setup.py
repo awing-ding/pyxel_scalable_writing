73f1f97000a133dca9b75eee111275c62a7df7fa
awing_ding <awingding.win@gmail.com>
2022-05-31 19:47:20 +0200

    1.2.1 forgot an file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup (
     name = "scalablewritingpyxel",
-    version = "1.1.6",
+    version = "1.2.0",
     description = "A module that give a scalable text writing function for pyxel",
     long_description = "A module that give a scalable text writing function for pyxel, see README.md on github for documentation",
     url = "https://github.com/awing-ding/pyxel_scalable_writing",
     author = "awing-ding",
     email = "awingding.win@gmail.com",
     classifiers = [
-        "Development Status :: 5 - Stable",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3 ",
     ],
