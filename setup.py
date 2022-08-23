7cc554f151e6d64a278129c81bd5d88d388cd28f
awing_ding <awingding.win@gmail.com>
2022-08-23 17:55:46 +0200

    fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here /'README.md').read_text(encoding="utf-8")
 
 setup (
     name = "scalablewritingpyxel",
-    version = "1.3.3",
+    version = "1.3.5",
     description = "A module that give a scalable text writing function for pyxel",
     long_description = long_description,
     long_description_content_type = "text/markdown",
@@ -19,6 +19,6 @@ setup (
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3 ",
     ],
-    py_modules=["__main__"],
+    py_modules=["scalablewritingpyxel"],
     install_requires = "pyxel",
 )
